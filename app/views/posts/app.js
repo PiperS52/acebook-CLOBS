1b8f08695061cfe789d39b922792b3678cfe29f6
Benedict Lawrence <BTLawrence44@gmail.com>
2021-01-28 15:59:44 +0000

    first commit

--- app/views/posts/app.js
+++ app/views/posts/app.js
@@ -3,11 +3,18 @@ import React, {Component} from 'react';
 
 class NewPost extends Component {
   
-  render() {
-    return <h1>THIS IS TEXT</h1>;
-  }
+  //render() {
+   // return <h1>THIS IS TEXT</h1>;
+  //} 
+  
+} 
 
-}
+
+import React from 'react'
+
+
+export default app
+
 
 ReactDOM.render(
   <NewPost />,
