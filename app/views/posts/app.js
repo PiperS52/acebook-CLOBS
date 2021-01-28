d8e52ae260256c7b14135e5d51b182fb0074818a
Luke Wickens <lukewickens1989@gmail.com>
2021-01-28 15:30:16 +0000

    Attempted to add React

---
+++ app/views/posts/app.js
@@ -0,0 +1,18 @@
+'use strict'
+import React, {Component} from 'react';
+
+class NewPost extends Component {
+  
+  render() {
+    return <h1>THIS IS TEXT</h1>;
+  }
+
+}
+
+ReactDOM.render(
+  <NewPost />,
+  document.getElementById('root')
+);
+
+export default NewPost;
+
